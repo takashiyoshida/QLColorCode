061bcd7eda0b6eb3ceebb42f59b7ecf5a5c59855
Anthony Gelibert <anthony.gelibert@me.com>
2012-12-05 13:42:46 +0100

    Revert "Don't process DWG files."
    This reverts commit 74a8f5f6c99cec860c58afad6a5228833c2aace9.

--- src/colorize.sh
+++ src/colorize.sh
@@ -47,10 +47,6 @@ case $target in
         # some omnigraffle files are XML and get passed to us.  Ignore them.
         exit 1
         ;;
-    *.dwg )
-        # Highlight can't process them.
-        exit 1
-        ;;
     *.plist )
         lang=xml
         reader=(/usr/bin/plutil -convert xml1 -o - $target)

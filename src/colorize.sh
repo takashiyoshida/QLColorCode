1a595c90d832a5a849791698cab21ae3c73312ad
Anthony Gelibert <anthony.gelibert@me.com>
2012-12-06 22:53:00 +0100

    Support PDE files.

--- src/colorize.sh
+++ src/colorize.sh
@@ -47,6 +47,9 @@ case $target in
         # some omnigraffle files are XML and get passed to us.  Ignore them.
         exit 1
         ;;
+    *.pde )
+        lang=c
+        ;;
     *.plist )
         lang=xml
         reader=(/usr/bin/plutil -convert xml1 -o - $target)
